91bc90131455e99d138d32d4005a9f14bc4752e4
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2020-07-14 00:33:50 +0600

    book now add

--- js/main.js
+++ js/main.js
@@ -5,8 +5,21 @@
         $('.popup-video').magnificPopup({
             type: 'iframe'
         });
-    
-    
+
+        // datepicker
+        $( "#datepicker" ).datepicker();
+
+        // datepicker toogle class
+        $("input").focus(function(){
+            $(".dateSelect").addClass("open");
+          });
+        $("input").focusout(function(){
+            $(".dateSelect").removeClass("open");
+          });
+        
+        //nice select
+        $('select').niceSelect();
+        
     
     
     
