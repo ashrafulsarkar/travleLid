d20fc1759e5600dffcdfaff06c559fa2dda9e3a5
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2020-07-24 03:18:59 +0600

    Top Destination

--- js/main.js
+++ js/main.js
@@ -19,6 +19,30 @@
         
         //nice select
         $('select').niceSelect();
+
+            // isotope masonary
+    var $grid = $('.grid').isotope({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      masonry: {
+          // use outer width of grid-sizer for columnWidth
+          columnWidth: '.grid-item'
+      }
+  });
+      // project menu active
+      $('.destination-menu').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
+        });
+    });
+
+    //for menu active class
+    $('.destination-menu button').on('click', function (event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
         
     
     
