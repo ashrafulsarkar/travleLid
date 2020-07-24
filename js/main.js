214e14694773eea1db732e690258826d66276655
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2020-07-24 17:44:04 +0600

    special offer add

--- js/main.js
+++ js/main.js
@@ -1,36 +1,36 @@
 (function ($) {
     "use strict";
-    
-        //    magnificPopup
-        $('.popup-video').magnificPopup({
-            type: 'iframe'
-        });
 
-        // datepicker
-        $( "#datepicker" ).datepicker();
+    //    magnificPopup
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+    });
 
-        // datepicker toogle class
-        $("input").focus(function(){
-            $(".dateSelect").addClass("open");
-          });
-        $("input").focusout(function(){
-            $(".dateSelect").removeClass("open");
-          });
-        
-        //nice select
-        $('select').niceSelect();
+    // datepicker
+    $("#datepicker").datepicker();
 
-            // isotope masonary
+    // datepicker toogle class
+    $("input").focus(function () {
+        $(".dateSelect").addClass("open");
+    });
+    $("input").focusout(function () {
+        $(".dateSelect").removeClass("open");
+    });
+
+    //nice select
+    $('select').niceSelect();
+
+    // isotope masonary
     var $grid = $('.grid').isotope({
-      itemSelector: '.grid-item',
-      percentPosition: true,
-      masonry: {
-          // use outer width of grid-sizer for columnWidth
-          columnWidth: '.grid-item'
-      }
-  });
-      // project menu active
-      $('.destination-menu').on('click', 'button', function () {
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            // use outer width of grid-sizer for columnWidth
+            columnWidth: '.grid-item'
+        }
+    });
+    // project menu active
+    $('.destination-menu').on('click', 'button', function () {
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({
             filter: filterValue
@@ -43,12 +43,47 @@
         $(this).addClass('active');
         event.preventDefault();
     });
-        
-    
-    
-    
-    
-    
-    
-    
+
+    //    spacial active
+    $('.spacial-active').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+
+
+
+
+
+
+
 })(jQuery);
