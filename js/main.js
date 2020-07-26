5abbcd7d734670ca0a4012ae92765c496d083441
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2020-07-27 00:38:39 +0600

    testimonial and blog add

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@
     $('.popup-video').magnificPopup({
         type: 'iframe'
     });
+    $('.blog-popup-video').magnificPopup({
+        type: 'iframe'
+    });
 
     // datepicker
     $("#datepicker").datepicker();
@@ -86,6 +89,75 @@
         arrows: true,
         speed: 300,
         slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    //    testimonial active
+    $('.testimonial-active').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    //    Travel Blog active
+    $('.blog-active').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
