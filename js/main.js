119091fffaaa0542c24e5c8c858ce54144fdd587
ashrafulsarkar <ashrafulsarkar47@gmail.com>
2020-08-02 20:54:26 +0600

    hover problem solve & scroll to top add

--- js/main.js
+++ js/main.js
@@ -164,7 +164,19 @@
             }
         }]
     });
-
+    // Scroll to Top 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 3000) {
+            $('#return-to-top').fadeIn(300);
+        } else {
+            $('#return-to-top').fadeOut(300);
+        }
+    });
+    $('#return-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+    });
 
 
 
